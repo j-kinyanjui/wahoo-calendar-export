1dd769567d05750beced132ed8abeb98accc3682
jkinyanjui <joseph.kinyanjui@codecentric.de>
2024-02-17 20:13:38 +0100

    init commit

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "wahoo-plan-to-calendar"
---
+++ src/main/kotlin/nesski/de/plugins/RestClient.kt
@@ -0,0 +1,13 @@
+package nesski.de.plugins
+
+import io.ktor.client.HttpClient
+import io.ktor.client.engine.cio.CIO
+import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
+import io.ktor.serialization.kotlinx.json.json
+
+val applicationHttpClient =
+    HttpClient(CIO) {
+        install(ContentNegotiation) {
+            json()
+        }
+    }
